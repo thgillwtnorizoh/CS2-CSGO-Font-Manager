41932f2dc6f4b0121deabe898c4b0a38d6726c16
thgillwtnorizoh <81919671+thgillwtnorizoh@users.noreply.github.com>
2026-08-26 08:03:07 +0700

    Use centralized 4.0.2 assembly identity

--- CSGO Font Manager/Properties/AssemblyInfo.cs
+++ CSGO Font Manager/Properties/AssemblyInfo.cs
@@ -1,36 +1,17 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CSGO Font Manager")]
-[assembly: AssemblyDescription("A font management tool for CS:GO")]
+[assembly: AssemblyTitle("CS2 + CS:GO Font Manager")]
+[assembly: AssemblyDescription("Font management tool for Counter-Strike 2 and legacy Counter-Strike: Global Offensive")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dotch")]
-[assembly: AssemblyProduct("CSGO Font Manager")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCompany("thgillwtnorizoh")]
+[assembly: AssemblyProduct("CS2 + CS:GO Font Manager")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bc394594-0adc-43a7-878d-c129d28e1143")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.1.0")]
-[assembly: AssemblyFileVersion("4.0.1.0")]
+[assembly: AssemblyVersion(CSGO_Font_Manager.ReleaseInfo.AssemblyVersion)]
+[assembly: AssemblyFileVersion(CSGO_Font_Manager.ReleaseInfo.AssemblyVersion)]
